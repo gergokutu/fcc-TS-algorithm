0a4f18fa7cb0b27dfcc9a324d69ff5f2c50b9305
gergokutu <gergo.kutu@gmail.com>
2020-12-04 16:52:43 +0100

    Implement finderKeepers

---
+++ finderKeepers.ts
@@ -0,0 +1,19 @@
+// Create a function that looks through an array arr
+// and returns the first element in it that passes a 'truth test'.
+// This means that given an element x, the 'truth test' is passed if func(x) is true.
+// If no element passes the test, return undefined.
+
+interface IfindElement {
+	arr: number[];
+	func: (num: number) => boolean;
+}
+
+const findElement = (params: IfindElement): number => {
+	const { arr, func } = params;
+	return arr.find(func);
+};
+
+console.log(
+	'Should be 2 >',
+	findElement({ arr: [1, 2, 3, 4], func: num => num % 2 === 0 })
+);
