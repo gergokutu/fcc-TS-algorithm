8822ea8feb442a0115d628f3b8a2c04d8415d5f3
Subash Pradahan <suprowilde5@gmail.com>
2020-12-04 15:48:23 +0100

    Solutions to truncate a string

---
+++ truncateString.ts
@@ -0,0 +1,14 @@
+// Truncate a string (first argument) if it is longer than the given maximum string length
+//  (second argument). Return the truncated string with a ... ending.
+
+interface IParams {
+  str: string;
+  num: number;
+}
+
+const truncateString = ({ str, num }: IParams): string => {
+  const res: string = str.slice(0, num);
+  return str.length > num ? res + '...' : str;
+}
+
+console.log("Should be A-tisket... >", truncateString({ str: "A-tisket a-tasket A green and yellow basket", num: 8 }));
