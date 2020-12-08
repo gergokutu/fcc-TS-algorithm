5d46a0657cc1fa0ecb7dc49b5fce5fe411a11c31
Subash Pradahan <suprowilde5@gmail.com>
2020-12-08 11:08:52 +0100

    Solution to BooWho

---
+++ booWho.ts
@@ -0,0 +1,8 @@
+// Check if a value is classified as a boolean primitive. Return true or false.
+// Boolean primitives are true and false.
+
+const booWho = (bool: any): boolean => {
+  return typeof bool !== 'boolean' ? false : true;
+}
+
+console.log('Should be > false', booWho(5));
