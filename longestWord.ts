6dcf6167b6b82d7c710ee2c7dcfcf0a6170678e8
gergokutu <gergo.kutu@gmail.com>
2020-12-08 10:49:45 +0100

    Add comments

--- longestWord.ts
+++ longestWord.ts
@@ -1,6 +1,7 @@
 // Return the length of the longest word in the provided sentence.
 // Your response should be a number.
 const findLongestWordLength = (str: string): number => {
+	// Use type assertion to say you won't get back undefined as the result of match()
 	const arrOfWords: RegExpMatchArray = str.match(/\w+/g) as RegExpMatchArray;
 	// const arrOfWords: RegExpMatchArray = <RegExpMatchArray>str.match(/\w+/g);
 	const resArr: number[] = arrOfWords.map(word => word.length);
