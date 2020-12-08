06bb39cad2375d57e740ea896fd15edcc4403480
gergokutu <gergo.kutu@gmail.com>
2020-12-08 11:41:13 +0100

    Refactor titleCase

--- titleCase.ts
+++ titleCase.ts
@@ -1,9 +1,8 @@
 // Return the provided string with the first letter of each word capitalized.
 const titleCase = (str: string): string => {
-  const myRegExp: RegExp = new RegExp(/^\w/);
   const words: string[] = str.toLowerCase().split(/\s/);
   const capitalFirst: string[] = words.map((word) => {
-    return word.replace(myRegExp, (match) => match.toUpperCase());
+    return word.replace(/^\w/, (match) => match.toUpperCase());
   });
   return capitalFirst.join(' ');
 };
