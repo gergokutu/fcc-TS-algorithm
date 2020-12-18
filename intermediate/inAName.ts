580d938cc9bf493db9110a517054b4e1643ce54b
Subash Pradahan <suprowilde5@gmail.com>
2020-12-18 13:20:08 +0100

    Factorizing inAName

--- intermediate/inAName.ts
+++ intermediate/inAName.ts
@@ -1,30 +1,50 @@
-// Make a function that looks through an array of objects (first argument)
-// Returns an array of all objects that have matching name and value pairs (second argument).
-// Each name and value pair of the source object has to be present in the object from the collection if it is to be included in the returned array.
+// // Make a function that looks through an array of objects (first argument)
+// // Returns an array of all objects that have matching name and value pairs (second argument).
+// // Each name and value pair of the source object has to be present in the object from the collection if it is to be included in the returned array.
 
+// V1 Using interface.
+// interface IName {
+//   collection: ICol[];
+//   source: ICol;
+// }
 
-interface IName {
-  collection: ICol[];
-  source: ICol;
+// interface ICol {
+//   [fieldName: string]: string | number | null;
+// }
+
+// const whatIsInAName = (params: IName): object[] => {
+//   const { collection, source } = params;
+//   const arr: string[] = Object.keys(source);
+
+//   return collection.filter((col: ICol) => {
+//     return arr.every(
+//       (src: string) => col.hasOwnProperty(src) && source[src] === col[src]
+//     );
+//   });
+// };
+
+// V2 Using Generics
+interface IName<T, U> {
+  collection: T[];
+  source: object;
 }
 
-interface ICol {
-  [fieldName: string]: string | number | null;
-}
-
-const whatIsInAName = (params: IName): object[] => {
+const whatIsInAName = <T extends object, U extends object>(
+  params: IName<T, U>
+): T[] => {
   const { collection, source } = params;
   const arr: string[] = Object.keys(source);
-
-  return collection.filter((col: ICol) => {
+  return collection.filter((col: T) => {
     return arr.every(
-      (src: string) => col.hasOwnProperty(src) && source[src] === col[src]
+      (src: string) =>
+        col.hasOwnProperty(src) &&
+        source[src as keyof typeof source] === col[src as keyof typeof col]
     );
   });
 };
 
 console.log(
-  "Should be [ { first: 'Romeo', last: 'Montague' } ] >",
+  "Should be [ { first: 'Tybalt', last: 'Capulet' } ] >",
   whatIsInAName({
     collection: [
       { first: 'Romeo', last: 'Montague' },
@@ -32,5 +52,31 @@ console.log(
       { first: 'Tybalt', last: 'Capulet' }
     ],
     source: { last: 'Capulet' }
+  })
+);
+
+console.log(
+  'should be [{ "apple": 1, "bat": 2 }, { "apple": 1, "bat": 2, "cookie": 2 }] >',
+  whatIsInAName({
+    collection: [
+      { apple: 1, bat: 2 },
+      { bat: 2 },
+      { apple: 1, bat: 2, cookie: 2 }
+    ],
+    source: { bat: 2, apple: 1 }
+  })
+);
+//
+
+console.log(
+  'should be [{ "apple": 1, "bat": 2 }, { "apple": 1, "bat": 2, "cookie":2 }] >',
+  whatIsInAName({
+    collection: [
+      { apple: 1, bat: 2 },
+      { apple: 1 },
+      { apple: 1, bat: 2, cookie: 2 },
+      { bat: 2 }
+    ],
+    source: { apple: 1, bat: 2 }
   })
 );
