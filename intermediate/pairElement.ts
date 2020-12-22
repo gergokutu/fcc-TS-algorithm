c33e5cd58930fcb99eb8d4d9aadd1bae41c19745
gergokutu <gergo.kutu@gmail.com>
2020-12-22 16:06:38 +0100

    Implement pairElements with switch structure

--- intermediate/pairElement.ts
+++ intermediate/pairElement.ts
@@ -4,24 +4,42 @@
 // For example, for the input GCG, return [["G", "C"], ["C","G"],["G", "C"]]
 // The character and its pair are paired up in an array, and all the arrays are grouped into one encapsulating array.
 
-const DnaPairs: myObject = {
-  A: 'T',
-  T: 'A',
-  C: 'G',
-  G: 'C'
-};
+// const DnaPairs: myObject = {
+//   A: 'T',
+//   T: 'A',
+//   C: 'G',
+//   G: 'C'
+// };
 
-interface myObject {
-  [key: string]: string;
-}
+// interface myObject {
+//   [key: string]: string;
+// }
 
-//V1
+// //V1
+// const pairElement = (str: string): string[][] => {
+//   const arrOfLetters: string[] = str.split('');
+//   return arrOfLetters.map(letter => [letter, DnaPairs[letter]]);
+// };
+
+// v2
 const pairElement = (str: string): string[][] => {
-  const arrOfLetters: string[] = str.split('');
-  return arrOfLetters.map(letter => [letter, DnaPairs[letter]]);
+  return str.split('').map(letter => {
+    switch (letter) {
+      case 'A':
+        return [letter, 'T'];
+      case 'T':
+        return [letter, 'A'];
+      case 'C':
+        return [letter, 'G'];
+      case 'G':
+        return [letter, 'C'];
+      default:
+        return [letter, 'Non-valid dna passed'];
+    }
+  });
 };
 
 console.log(
-  'Should be [["A","T"],["T","A"],["C","G"],["G","C"],["A","T"]] >',
-  pairElement('ATCGA')
+  'Should be [["X","Non-valid dna passed"],["T","A"],["C","G"],["G","C"],["A","T"]] >',
+  pairElement('XTCGA')
 );
