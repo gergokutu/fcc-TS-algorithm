dd39d01867b3e7271c20f8766074d7a61d63729f
gergokutu <gergo.kutu@gmail.com>
2020-12-22 11:22:51 +0100

    Start implementing another version

--- intermediate/spinalCase.ts
+++ intermediate/spinalCase.ts
@@ -1,5 +1,15 @@
 // Convert a string to spinal case.
 // Spinal case is all-lowercase-words-joined-by-dashes.
+
+// v1
+// const spinalCase = (str: string): string => {
+//   return str
+//     .trim()
+//     .replace(/_|\s+|(?<=[A-Za-z0-9])(?=[A-Z])/g, '-')
+//     .toLowerCase();
+// };
+
+// v2
 const spinalCase = (str: string): string => {
   return str
     .trim()
