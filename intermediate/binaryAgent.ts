67e7fc7a8f0ea8076840ed32e2ab5149e5302c2e
gergokutu <gergo.kutu@gmail.com>
2021-01-08 10:51:33 +0100

    Implement binaryAgent

---
+++ intermediate/binaryAgent.ts
@@ -0,0 +1,13 @@
+const binaryAgent = (str: string): string => {
+  return str
+    .split(/\s/)
+    .map(binary => String.fromCharCode(parseInt(binary, 2)))
+    .join('');
+};
+
+console.log(
+  " Should be Aren't bonfires fun!? >",
+  binaryAgent(
+    '01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111'
+  )
+);
